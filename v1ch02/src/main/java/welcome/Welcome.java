eeaf86de6b4ad95347567c18574a47f118ea0359
kotohiko <tachibana.kotohiko@gmail.com>
2024-05-22 19:02:15 +0800

    更新

--- v1ch02/src/main/java/welcome/Welcome.java
+++ v1ch02/src/main/java/welcome/Welcome.java
@@ -10,7 +10,7 @@ public class Welcome {
     public static void main(String[] args) {
         String greeting = "Welcome to Core Java!";
         System.out.println(greeting);
-        for (int i = 0; i < greeting.length(); i++) {
+        for (int i = 0; i < greeting.length(); ++i) {
             System.out.print("=");
         }
         System.out.println();
